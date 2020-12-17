19a96cc495e1b921e2dda0e58b0752aded1a9377
nrodrigoc <nathan--rodrigo@hotmail.com>
2020-12-17 15:55:38 -0300

    Post method no front

--- logistica-front/src/components/Form.jsx
+++ logistica-front/src/components/Form.jsx
@@ -1,31 +1,54 @@
 import React, { useState, useEffect } from "react";
 import api from "../api/api";
-import caminhoneiro from "../api/caminhoneiro"
+import caminhoneiro from "../api/caminhoneiro";
 
 function Form() {
-  const [nome, setNome] = useState("");
-  const [id, setId] = useState(-1);
+    //Produto
+    const [nomeProduto, setNomeProduto] = useState("");
+    const [id, setId] = useState(-1);
 
-  useEffect(() => {
-    api.get("/produtos/1").then((response) => {
-      const data = response.data;
-      setId(data.id);
-      setNome(data.nome);
-    });
-  }, []);
+    //Caminhoneiro
+    const [nomeCaminhoneiro, setNomeCaminhoneiro] = useState("");
 
-  return (
-    <>
-      <h3>Adicionar caminhoneiro</h3>
-      <br />
-      <form>
-        <label for="fname">Nome: </label>
-        <input type="text" id="fname" name="fname"/><br/>
-      </form>
-      <label>Id do produto: {id}</label> <br />
-      <label>Nome do produto: {nome}</label>
-    </>
-  );
+    // GET-Method
+    useEffect(() => {
+        api.get("/produtos/1").then((response) => {
+            const data = response.data;
+            setId(data.id);
+            setNomeProduto(data.nomeProduto);
+        });
+    }, []);
+
+    // POST-Method
+    function addCaminhoneiro() {
+        caminhoneiro
+            .post("http://localhost:8080/caminhoneiro/", {
+                nome: nomeCaminhoneiro,
+            })
+            .then((response) => console.log(response))
+            .catch((error) => console.log(error));
+    }
+
+    function handleNomeCaminhoneiro(c_nome) {
+        setNomeCaminhoneiro(c_nome);
+    }
+
+    return (
+        <>
+            <h3>Adicionar caminhoneiro</h3>
+            <br />
+            <input
+                type="text"
+                onChange={(e) => handleNomeCaminhoneiro(e.target.value)}
+            />
+            <button type="button" onClick={() => addCaminhoneiro()}>
+                Enviar
+            </button>
+            <br />
+            <label>Id do produto: {id}</label> <br />
+            <label>nomeProduto do produto: {nomeProduto}</label>
+        </>
+    );
 }
 
 export default Form;
